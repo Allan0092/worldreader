af4db74d7303ac0b16e8eb1cddf4f82190c7908b
allan <allan.gtm92@gmail.com>
2024-12-08 22:08:01 +0545

    refactored login page layout for improved responsiveness when phone is flipped

--- lib/view/sign_up_page.dart
+++ lib/view/sign_up_page.dart
@@ -18,129 +18,133 @@ class _SignUpPageState extends State<SignUpPage> {
             'assets/images/worldmap_login_bg.png',
             fit: BoxFit.cover,
           ),
-          Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/icons/WorldReaderLogo.png',
-                height: 150,
-              ),
-              const SizedBox(height: 20),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Email",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintStyle: TextStyle(
-                        fontFamily: "Montserrat",
-                        fontSize: 15,
-                        color: const Color(0xFF36454F).withOpacity(0.5)),
-                    hintText: 'example@email.com',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Password",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintStyle: TextStyle(
-                        color: const Color(0xFF36454F).withOpacity(0.5)),
-                    hintText: '*******',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Confirm Password",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintStyle: TextStyle(
-                        color: const Color(0xFF36454F).withOpacity(0.5)),
-                    hintText: '*******',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          const Color(0xFF009CDA), // Blue background
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0))),
-                  child: const Text("Submit")),
-              const SizedBox(height: 40),
-              Row(
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Center(
+                  child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    "Log in with  ",
-                    style: TextStyle(color: Colors.white),
-                  ),
                   Image.asset(
-                    "assets/icons/login_with_google_icon.png",
-                    height: 19,
+                    'assets/icons/WorldReaderLogo.png',
+                    height: 150,
                   ),
-                  const Padding(padding: EdgeInsets.all(3)),
-                  Image.asset(
-                    "assets/icons/login_with_facebook_icon.png",
-                    height: 19,
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Email",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
                   ),
-                  const Padding(padding: EdgeInsets.all(3)),
-                  Image.asset(
-                    "assets/icons/login_with_github_icon.png",
-                    height: 19,
+                  TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintStyle: TextStyle(
+                            fontFamily: "Montserrat",
+                            fontSize: 15,
+                            color: const Color(0xFF36454F).withOpacity(0.5)),
+                        hintText: 'example@email.com',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    keyboardType: TextInputType.emailAddress,
                   ),
-                ],
-              ),
-              const SizedBox(height: 30),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an account? ",
-                    style: TextStyle(color: Colors.white),
+                  const SizedBox(
+                    height: 10,
                   ),
-                  Text(
-                    "Log in",
-                    style: TextStyle(color: Colors.blue),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Password",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintStyle: TextStyle(
+                            color: const Color(0xFF36454F).withOpacity(0.5)),
+                        hintText: '*******',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Confirm Password",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintStyle: TextStyle(
+                            color: const Color(0xFF36454F).withOpacity(0.5)),
+                        hintText: '*******',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color(0xFF009CDA), // Blue background
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0))),
+                      child: const Text("Submit")),
+                  const SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Log in with  ",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Image.asset(
+                        "assets/icons/login_with_google_icon.png",
+                        height: 19,
+                      ),
+                      const Padding(padding: EdgeInsets.all(3)),
+                      Image.asset(
+                        "assets/icons/login_with_facebook_icon.png",
+                        height: 19,
+                      ),
+                      const Padding(padding: EdgeInsets.all(3)),
+                      Image.asset(
+                        "assets/icons/login_with_github_icon.png",
+                        height: 19,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account? ",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "Log in",
+                        style: TextStyle(color: Colors.blue),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
-          ))
+              )),
+            ),
+          )
         ],
       ),
     );
