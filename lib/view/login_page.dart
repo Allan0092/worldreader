038d567310eb68984069d4cebcd0d254e30183dc
allan <allan.gtm92@gmail.com>
2024-12-08 20:58:47 +0545

    completed logo and styling

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -22,16 +22,62 @@ class _LoginPageState extends State<LoginPage> {
               child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                decoration: InputDecoration(hintText: 'Email'),
+              Image.asset(
+                'assets/icons/WorldReaderLogo.png',
+                height: 200,
+              ),
+              const SizedBox(height: 20),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Email",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
               ),
               TextField(
                 decoration: InputDecoration(
-                  hintText: 'Password',
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintStyle: TextStyle(
+                        color: const Color(0xFF36454F).withOpacity(0.5)),
+                    hintText: 'example@email.com',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Password",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
                 ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintStyle: TextStyle(
+                        color: const Color(0xFF36454F).withOpacity(0.5)),
+                    hintText: '*******',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
                 obscureText: true,
               ),
-              ElevatedButton(onPressed: () {}, child: Text("Log in"))
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xFF009CDA), // Blue background
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0))),
+                  child: const Text("Log in"))
             ],
           ))
         ],
