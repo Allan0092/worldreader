1d90d12c3a48ffe7e4d949c390d809b19baf36a3
allan <allan.gtm92@gmail.com>
2024-12-12 21:53:18 +0545

    refactored login page to use form validation and integrated showBottomSnackBar for feedback

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:worldreader/common/show_bottom_snack_bar.dart';
 import 'package:worldreader/view/sign_up_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,6 +10,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController(text: "");
+  final passwordController = TextEditingController(text: "");
+  final myKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,117 +25,148 @@ class _LoginPageState extends State<LoginPage> {
             fit: BoxFit.cover,
           ),
           SafeArea(
-            child: SingleChildScrollView(
-              child: Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/icons/WorldReaderLogo.png',
-                    height: 200,
-                  ),
-                  const SizedBox(height: 20),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Email",
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+            child: Form(
+              key: myKey,
+              child: SingleChildScrollView(
+                child: Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/icons/WorldReaderLogo.png',
+                      height: 200,
                     ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintStyle: TextStyle(
-                            fontFamily: "Montserrat",
-                            fontSize: 15,
-                            color: const Color(0xFF36454F).withOpacity(0.5)),
-                        hintText: 'example@email.com',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Password",
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    const SizedBox(height: 20),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Email",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
                     ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintStyle: TextStyle(
-                            color: const Color(0xFF36454F).withOpacity(0.5)),
-                        hintText: '*******',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                    obscureText: true,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color(0xFF009CDA), // Blue background
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0))),
-                      child: const Text("Log in")),
-                  const SizedBox(height: 40),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Log in with  ",
-                        style: TextStyle(color: Colors.white),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintStyle: TextStyle(
+                              fontFamily: "Montserrat",
+                              fontSize: 15,
+                              color: const Color(0xFF36454F).withOpacity(0.5)),
+                          hintText: 'example@email.com',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "enter an email";
+                        } else if (value == "admin@email.com") {
+                          return null;
+                        }
+                        return "email not registered";
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Password",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
                       ),
-                      Image.asset(
-                        "assets/icons/login_with_google_icon.png",
-                        height: 19,
-                      ),
-                      const Padding(padding: EdgeInsets.all(3)),
-                      Image.asset(
-                        "assets/icons/login_with_facebook_icon.png",
-                        height: 19,
-                      ),
-                      const Padding(padding: EdgeInsets.all(3)),
-                      Image.asset(
-                        "assets/icons/login_with_github_icon.png",
-                        height: 19,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Create new account? ",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignUpPage()));
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintStyle: TextStyle(
+                              color: const Color(0xFF36454F).withOpacity(0.5)),
+                          hintText: '*******',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "enter a password";
+                        } else if (value == "admin") {
+                          return null; // correct creds
+                        }
+                        return "incorrect password";
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (myKey.currentState!.validate()) {
+                            showBottomSnackBar(
+                                context: context, message: "Login Success");
+                          } else {
+                            showBottomSnackBar(
+                                context: context,
+                                message: "Login error",
+                                color: Colors.red);
+                          }
                         },
-                        child: const Text(
-                          "Sign up",
-                          style: TextStyle(color: Colors.blue),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color(0xFF009CDA), // Blue background
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0))),
+                        child: const Text("Log in")),
+                    const SizedBox(height: 40),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Log in with  ",
+                          style: TextStyle(color: Colors.white),
                         ),
-                      )
-                    ],
-                  )
-                ],
-              )),
+                        Image.asset(
+                          "assets/icons/login_with_google_icon.png",
+                          height: 19,
+                        ),
+                        const Padding(padding: EdgeInsets.all(3)),
+                        Image.asset(
+                          "assets/icons/login_with_facebook_icon.png",
+                          height: 19,
+                        ),
+                        const Padding(padding: EdgeInsets.all(3)),
+                        Image.asset(
+                          "assets/icons/login_with_github_icon.png",
+                          height: 19,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Create new account? ",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const SignUpPage()));
+                          },
+                          child: const Text(
+                            "Sign up",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )),
+              ),
             ),
           )
         ],
