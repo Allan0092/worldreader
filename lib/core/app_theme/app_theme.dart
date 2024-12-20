551a506ed6e434a66bedb0ee679dda5e5f8f848d
allan <allan.gtm92@gmail.com>
2024-12-20 16:34:35 +0545

    created a global app theme

--- lib/core/app_theme/app_theme.dart
+++ lib/core/app_theme/app_theme.dart
@@ -2,5 +2,34 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      fontFamily: "Montserrat Regular", scaffoldBackgroundColor: Colors.amber);
+      fontFamily: "Montserrat Regular",
+      scaffoldBackgroundColor: Colors.black45,
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Colors.white),
+        bodyMedium: TextStyle(color: Colors.white),
+        displayLarge: TextStyle(color: Colors.white),
+        displayMedium: TextStyle(color: Colors.white),
+        displaySmall: TextStyle(color: Colors.white),
+        headlineMedium: TextStyle(color: Colors.white),
+        headlineSmall: TextStyle(color: Colors.white),
+        titleLarge: TextStyle(color: Colors.white),
+        titleMedium: TextStyle(color: Colors.white),
+        titleSmall: TextStyle(color: Colors.white),
+        bodySmall: TextStyle(color: Colors.white),
+        labelLarge: TextStyle(color: Colors.white),
+        labelSmall: TextStyle(color: Colors.white),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            textStyle: const TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontFamily: 'Montserrat Bold'),
+            backgroundColor: const Color(0xFF009CDA), // Blue background
+            foregroundColor: Colors.white,
+            shadowColor: Colors.black,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0))),
+      ));
 }
