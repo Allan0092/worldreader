cd87870296b99a6a980c7c0a27ff340d2bbc9d5e
allan <allan.gtm92@gmail.com>
2025-01-24 12:20:13 +0545

    feat:  login and registration states, events, and blocs; initialize Hive service in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:worldreader/app/di/di.dart';
+import 'package:worldreader/core/network/hive_service.dart';
 
 import 'app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveService.init();
+  await initDependencies();
   runApp(
     const MyApp(),
   );
