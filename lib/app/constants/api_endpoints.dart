51b4862aee68d979d51a1ef9477528aab960c49b
allan <allan.gtm92@gmail.com>
2025-01-31 20:10:04 +0545

    fix: update API endpoints to remove redundant path segments and improve clarity

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -3,12 +3,12 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 5000);
   static const Duration receiveTimeout = Duration(seconds: 5000);
-  static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
+  static const String baseUrl = "http://10.0.2.2:3000/api/";
 // ============================ Auth Routes ==================================
 
-  static const String registerUser = "/api/user/"; // POST
-  static const String loginUser = "/api/user/login"; // POST
-  static const String updateUser = "/api/user/update"; // POST
-  static const String findUserByEmail = "/api/user/findByEmail"; // POST
-  static const String uploadImage = "/auth/uploadImage"; //
+  static const String registerUser = "user/"; // POST
+  static const String loginUser = "user/login"; // POST
+  static const String updateUser = "user/update"; // POST
+  static const String findUserByEmail = "user/findByEmail"; // POST
+  static const String uploadImage = "imageUpload"; //
 }
