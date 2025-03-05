30544d5f45930d9b112fb61708b04dd4b0bf9cfa
allan <allan.gtm92@gmail.com>
2025-03-05 12:58:23 +0545

    feat: add dependency injection for Store module components

--- lib/app/di/di.dart
+++ lib/app/di/di.dart
@@ -15,6 +15,12 @@ import 'package:worldreader/features/auth/presentation/view_model/login/login_bl
 import 'package:worldreader/features/auth/presentation/view_model/register/register_bloc.dart';
 import 'package:worldreader/features/on_boarding/presentation/view_model/on_boarding_screen_bloc.dart';
 import 'package:worldreader/features/splash/presentation/view_model/splash_cubit.dart';
+import 'package:worldreader/features/store/data/data_source/local_data_source/store_local_data_source.dart';
+import 'package:worldreader/features/store/data/data_source/remote_data_source/store_remote_data_source.dart';
+import 'package:worldreader/features/store/data/repository/store_local_repository/store_local_repository.dart';
+import 'package:worldreader/features/store/data/repository/store_remote_repository/store_remote_repository.dart';
+import 'package:worldreader/features/store/domain/use_case/get_all_books_usecase.dart';
+import 'package:worldreader/features/store/presentation/view_model/store_bloc.dart';
 
 final getIt = GetIt.instance;
 
@@ -28,6 +34,7 @@ Future<void> initDependencies() async {
   await _initLoginDependencies();
   await _initSplashScreenDependencies();
   await _initOnBoardingScreenDependencies();
+  await _initStoreDependency();
 }
 
 _initApiService() {
@@ -77,6 +84,7 @@ _initRegisterDependencies() {
   getIt.registerLazySingleton<UploadImageUsecase>(
       () => UploadImageUsecase(getIt<AuthRemoteRepository>()));
 
+  // Bloc
   getIt.registerFactory<RegisterBloc>(
     () => RegisterBloc(
       registerUseCase: getIt(),
@@ -96,6 +104,7 @@ _initLoginDependencies() async {
     () => TokenSharedPrefs(getIt<SharedPreferences>()),
   );
 
+  // UseCase
   getIt.registerLazySingleton<LoginUseCase>(
     () => LoginUseCase(
       getIt<AuthRemoteRepository>(),
@@ -103,6 +112,7 @@ _initLoginDependencies() async {
     ),
   );
 
+  // Bloc
   getIt.registerFactory<LoginBloc>(
     () => LoginBloc(
       registerBloc: getIt<RegisterBloc>(),
@@ -110,6 +120,30 @@ _initLoginDependencies() async {
       loginUseCase: getIt<LoginUseCase>(),
     ),
   );
+}
+
+_initStoreDependency() async {
+  // Data Sources
+  getIt.registerLazySingleton<StoreLocalDataSource>(
+      () => StoreLocalDataSource(getIt<HiveService>()));
+
+  getIt.registerLazySingleton<StoreRemoteDataSource>(
+      () => StoreRemoteDataSource(getIt<Dio>()));
+
+  // Repositories
+  getIt.registerLazySingleton<StoreLocalRepository>(() => StoreLocalRepository(
+      storeLocalDataSource: getIt<StoreLocalDataSource>()));
+
+  getIt.registerLazySingleton<StoreRemoteRepository>(
+      () => StoreRemoteRepository(getIt<StoreRemoteDataSource>()));
+
+  // UseCase
+  getIt.registerLazySingleton<GetAllBooksUseCase>(
+      () => GetAllBooksUseCase(repository: getIt<StoreRemoteRepository>()));
+
+  // Bloc
+  getIt.registerFactory<StoreBloc>(
+      () => StoreBloc(getAllBooksUseCase: getIt<GetAllBooksUseCase>()));
 }
 
 _initSplashScreenDependencies() async {
