a96ed650917fa322e89794650593a76a8bbb3e27
allan <allan.gtm92@gmail.com>
2025-03-05 09:18:16 +0545

    feat: implement StoreLocalDataSource for retrieving store books from Hive

---
+++ lib/features/store/data/data_source/local_data_source/store_local_data_source.dart
@@ -0,0 +1,22 @@
+import 'package:worldreader/core/network/hive_service.dart';
+import 'package:worldreader/features/store/data/data_source/store_data_source.dart';
+import 'package:worldreader/features/store/domain/entity/book_entity.dart';
+
+class StoreLocalDataSource implements IStoreDataSource {
+  final HiveService _hiveService;
+
+  StoreLocalDataSource(this._hiveService);
+
+  @override
+  Future<List<BookEntity>> getStoreBooks() {
+    try {
+      return _hiveService.getAllBooks().then(
+        (value) {
+          return value.map((e) => e.toEntity()).toList();
+        },
+      );
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
