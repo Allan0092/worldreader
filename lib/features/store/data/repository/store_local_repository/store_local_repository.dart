4de0a3824fdf970c00990f1eb8190e548501d6c6
allan <allan.gtm92@gmail.com>
2025-03-05 09:18:27 +0545

    feat: add StoreLocalRepository for managing local book data retrieval

---
+++ lib/features/store/data/repository/store_local_repository/store_local_repository.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:worldreader/core/error/failure.dart';
+import 'package:worldreader/features/store/data/data_source/local_data_source/store_local_data_source.dart';
+import 'package:worldreader/features/store/domain/entity/book_entity.dart';
+import 'package:worldreader/features/store/domain/repository/store_repository.dart';
+
+class StoreLocalRepository implements IStoreRepository {
+  final StoreLocalDataSource _storeLocalDataSource;
+
+  StoreLocalRepository({required StoreLocalDataSource storeLocalDataSource})
+      : _storeLocalDataSource = storeLocalDataSource;
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> getStoreBooks() {
+    try {
+      return _storeLocalDataSource.getStoreBooks().then((value) {
+        return Right(value);
+      });
+    } catch (e) {
+      return Future.value(Left(LocalDatabaseFailure(message: e.toString())));
+    }
+  }
+}
