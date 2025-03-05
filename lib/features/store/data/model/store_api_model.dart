3c8e15ff2db3f12528834ca18b786e4db471b33a
allan <allan.gtm92@gmail.com>
2025-03-05 08:34:42 +0545

    feat: add StoreApiModel for handling book data serialization

---
+++ lib/features/store/data/model/store_api_model.dart
@@ -0,0 +1,51 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:worldreader/features/store/domain/entity/book_entity.dart';
+
+part 'store_api_model.g.dart';
+
+@JsonSerializable()
+class StoreApiModel extends Equatable {
+  @JsonKey(name: "_id")
+  final String id;
+
+  final String title;
+  final String coverURL;
+  final String author;
+  final bool verifiedStatus;
+
+  const StoreApiModel({
+    required this.id,
+    required this.title,
+    required this.coverURL,
+    required this.author,
+    required this.verifiedStatus,
+  });
+
+  factory StoreApiModel.formJson(Map<String, dynamic> json) =>
+      _$StoreApiModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$StoreApiModelToJson(this);
+
+  BookEntity toEntity() {
+    return BookEntity(
+      id: id,
+      title: title,
+      author: author,
+      coverURL: coverURL,
+      verifiedStatus: verifiedStatus,
+    );
+  }
+
+  factory StoreApiModel.fromEntity(BookEntity entity) {
+    return StoreApiModel(
+        id: entity.id,
+        title: entity.title,
+        coverURL: entity.coverURL,
+        author: entity.author,
+        verifiedStatus: entity.verifiedStatus);
+  }
+
+  @override
+  List<Object?> get props => [id, title, coverURL, author, verifiedStatus];
+}
